e96c734215d48722a30ceff5a35ba35bfbc7e300
SamStha07 <samshrestha85@gmail.com>
2021-03-17 22:50:40 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
+const apiKeys = {
+  key: '424cc54aeb934f5e8b0154057211703',
+};
+
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+  const [error, setError] = useState('');
+
+  const getWeather = async (e) => {
+    e.preventDefault();
+
+    try {
+      const {
+        data,
+      } = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${apiKeys.key}&q=${query}
+        `);
+
+      setWeather(data);
+      setQuery('');
+      setError('');
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='form'>
+        <form>
+          <input
+            type='text'
+            id='city'
+            placeholder='Enter City Name'
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button type='submit' onClick={getWeather}>
+            Get Weather
+          </button>
+        </form>
+      </div>
+
+      {error ? (
+        <div className='error'>{error}</div>
+      ) : (
+        <>
+          <div className='title'>
+            {weather && weather.location && weather.location.name}
+          </div>
+          <div className='main'>
+            {weather && weather.current && (
+              <div>
+                <img src={weather.current.condition.icon} alt='' />
+                <div>Humidity: {weather.current.humidity}%</div>
+                <div>
+                  Temperature: {weather.current.temp_c}
+                  <span>&#8451;</span>
+                </div>
+                <div>Wind: {weather.current.wind_kph} km/h</div>
+              </div>
+            )}
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
